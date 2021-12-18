fd2afddfcb1d01f2fc6d57babf118da5cad402e0
Nikita Kuznetsov <me@daedalus.ru>
2021-12-18 23:23:26 +0300

    chore: cleanup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,3 @@
-import {Link} from "react-router-dom";
 import {gql, useMutation} from "@apollo/client";
 
 export const loginMutationGQL = gql`
@@ -18,25 +17,9 @@ export const LoginPage = ({loading}: {loading: boolean}) => {
         event.preventDefault();
         loginMutation({variables: {login: "x", password: "x"}});
       }}>
-        <h1 >
-          Nice to see you again, go ahead and log in
-        </h1>
-        <div>
-          <label>E-pasts</label>
-          <input name="username" placeholder="your@email.com" />
-        </div>
-        <div>
-          <label>Password</label>
-          <input name="password" type="password" placeholder="Password" />
-        </div>
         <div>
           <button
-            color="light_blue"
-            style={{width: "100%", margin: 0}}
-          />
-        </div>
-        <div>
-          <Link to="/forgot-password">Forgot password?</Link>
+            style={{width: "20%", margin: 0}}>CLICK TO TRIGGER</button>
         </div>
       </form>
     </div>
